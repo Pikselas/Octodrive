4c64de9a4ccba5d26f08538ca56453600a9a9042
kashikichi_Rin <60090083+Pikselas@users.noreply.github.com>
2022-10-09 02:10:06 +0000

    Read ,encode , return

---
+++ GithubAPI/RemoteReader.go
@@ -0,0 +1,28 @@
+package GithubAPI
+
+import (
+	"bytes"
+	"io"
+)
+
+type RemoteReader struct {
+	source            io.ReadCloser
+	encoder           io.WriteCloser
+	buffer            *bytes.Buffer
+	active_read_state bool
+}
+
+func (r *RemoteReader) Read(p []byte) (int, error) {
+	buff := make([]byte, 1000)
+	if r.active_read_state {
+		count, err := r.source.Read(buff)
+		r.encoder.Write(buff[:count])
+		if err == io.EOF {
+			r.active_read_state = false
+			r.encoder.Close()
+		}
+	}
+	count, err := r.buffer.Read(buff)
+	copy(p, buff[:count])
+	return count, err
+}
